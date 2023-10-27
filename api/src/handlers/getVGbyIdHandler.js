cee46856999e2c59910f1ef3d1f4a37329bd52b5
Elizabeth MIcaela Gurdzel <121364740+elizabethMica@users.noreply.github.com>
2023-10-27 16:56:05 -0300

    comments added

--- api/src/handlers/getVGbyIdHandler.js
+++ api/src/handlers/getVGbyIdHandler.js
@@ -1,14 +1,14 @@
-const {getVGbyId} = require("../controllers/getVGbyId");
+const {getVGbyId} = require("../controllers/getVGbyId"); // Import the controller function for getting a videogame by ID
 
-
+// Define an asynchronous request handler function for getting a videogame by its ID
 const getVGbyIdHandler =async(req, res)=>{
-  const id = req.params.id;
-  const source = isNaN(id)? "DB" : "API";
+  const id = req.params.id; // Extract the 'id' parameter from the request URL
+  const source = isNaN(id)? "DB" : "API"; // Determine the source of the videogame data (DB or API)
   try {
-    const vgId = await getVGbyId(id, source)
-    res.status(200).json(vgId)
+    const vgId = await getVGbyId(id, source) // Call the controller function to retrieve the videogame by ID
+    res.status(200).json(vgId) // Respond with a JSON object containing the videogame data
   } catch (error) {
-    res.status(400).send({error: error.message})
+    res.status(400).send({error: error.message}) // Handle any errors and respond with an error message
   }
 };
 
--- api/src/handlers/getVGhandler.js
+++ api/src/handlers/getVGhandler.js
@@ -1,13 +1,15 @@
-const { getAllVG } = require("../controllers/getVG");
-const {getVGbyName} = require("../controllers/getVGbyName");
+const { getAllVG } = require("../controllers/getVG");  // Import the controller function for getting all videogames
+const {getVGbyName} = require("../controllers/getVGbyName"); // Import the controller function for getting a videogame by name
 
-
+// Define an asynchronous request handler function for getting videogames
 const getVGhandler = async(req, res)=>{
   try {
-    const name = req.query.name
+    const name = req.query.name // Extract the 'name' query parameter from the request
+    // Check if 'name' is provided in the query parameter, if so, call the controller to get a video game by name; 
+    //otherwise, call the controller to get all videogames
     const response = name? await getVGbyName(name) : await getAllVG();
 
-    res.status(200).json(response)
+    res.status(200).json(response) // Respond with a JSON object containing the video game(s) data
 
   } catch (error) {
     res.status(404).send({error: error.message})
