0a0d48489a9dda989f3c969ffcf2d9518615dee0
Elizabeth MIcaela Gurdzel <121364740+elizabethMica@users.noreply.github.com>
2023-10-27 17:09:47 -0300

    comments added

--- api/src/controllers/postVG.js
+++ api/src/controllers/postVG.js
@@ -1,16 +1,18 @@
 const {Videogame, Genre} = require("../db");
 
+// Define an asynchronous controller function for creating a new videogame
 const postVG = async(req, res)=>{
    try {
     const imagen = "https://media.istockphoto.com/id/1303938743/es/foto/blanco-la-mejor-almohadilla-de-juego-flotando-sobre-fondo-blanco-idea-conceptual-m%C3%ADnima.jpg?s=612x612&w=0&k=20&c=_yJ_TOpSKZUu0dN7de7BCq--NFagk9xxRNUfiRVt06g="
 
     const {name, image, platforms, released, rating, genres, description} = req.body
 
+    // Find or create a new videogame in the database based on its name
     const [newVG, created] = await Videogame.findOrCreate({
         where: {name},
         defaults: {
             name: name,
-            image: image? image : imagen,
+            image: image? image : imagen, // Set the default image if not provided
             platforms: platforms,
             released: released,
             description: description,
@@ -18,16 +20,19 @@ const postVG = async(req, res)=>{
         }
     })
 
+    // If the videogame was created, associate it with genres and retrieve its details
     if(created === true){
         for(let i = 0; i<genres.length; i++){
+            // Find the genre by name and retrieve its ID
             const genreId = (await Genre.findOne({
                 where:{
                     name:genres[i]
                 }
             })).id
+            // Associate the videogame with the genre
             await newVG.addGenre(genreId)
         }
-    
+         // Retrieve the final details of the newly created videogame, including associated genres
         const finalNewVG = await Videogame.findOne({
             where:{id: newVG.id},
             include:[
@@ -36,9 +41,11 @@ const postVG = async(req, res)=>{
                 through: {attributes:[]}}
             ]
         })
+        // Respond with the newly created videogame details
         res.status(200).json(finalNewVG)
       
         }else{
+            // If the videogame already exists, return an error
             res.status(404).json({error:"The videogame already exists"})
         }
     
